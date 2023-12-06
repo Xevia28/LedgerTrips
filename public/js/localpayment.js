122df698e0436bf8c27128a2bcf68176e693d43c
Tandin Dorji <12210091.gcit@rub.edu.bt>
2023-12-07 00:25:00 +0600

    made the payment

--- public/js/localpayment.js
+++ public/js/localpayment.js
@@ -1,3 +1,15 @@
+const receiverAddress = "rLBG48rc8djjJV15Yn7Rssp8PHbCTsY9pe";
+const secretKey = "sEdVQ4htrwdfVywGXcHSmdNck5bCgSh";
+
+async function sendTransaction() {
+    const amount = parseFloat(bData.totalAmount.replace(/[^\d.-]/g, ''));
+    const exchangeRate = 0.75;
+
+    const xrplvalue = amount * exchangeRate
+    // Call the function with appropriate arguments
+    sendXRP("sEdSvTY3uM4YPJGfDEmdz64kTNBvgCQ", xrplvalue, receiverAddress);
+}
+
 async function bookNow(data) {
     const book = await axios({
         method: "POST",
@@ -5,7 +17,6 @@ async function bookNow(data) {
         data: data
     });
     loader.classList.add("hide")
-
     if (book.data.status === "success") {
         Swal.fire(
             '',
@@ -16,14 +27,11 @@ async function bookNow(data) {
                 location.assign("/");
             }, 100);
         });
-       
     }
-
 }
 
 var spanElement = document.querySelector('h3.amount span');
 const bData = JSON.parse(sessionStorage.getItem("bookingData"));
-
 spanElement.textContent = bData.totalAmount;
 
 const reservationID = bData.reservationID
@@ -41,35 +49,98 @@ const totalRoomTypes = bData.totalRoomTypes
 const totalRooms = bData.totalRooms
 const totalAmount = bData.totalAmount
 const loader = document.getElementById("loading-screen")
-const submitButton = document.getElementById("btn");
+
+// const submitButton = document.getElementById("btn");
+// document.getElementById("btn").addEventListener("click", async (e) => {
+//     e.preventDefault();
+//     loader.classList.remove("hide")
+//     const transactionID = document.getElementById("journalNumber").value
+//     if (transactionID == "") {
+//         Swal.fire(
+//             '',
+//             'Journal Number required!',
+//             'error'
+//         )
+//     } else {
+//         const data = {
+//             reservationID,
+//             name,
+//             email,
+//             contactNumber,
+//             specialRequirement,
+//             roomType,
+//             checkinDate,
+//             checkoutDate,
+//             adultNumber,
+//             childMinorNumber,
+//             childNumber,
+//             totalRoomTypes,
+//             totalRooms,
+//             totalAmount,
+//             transactionID
+//         }
+//         bookNow(data)
+//     }
+// })
+
 document.getElementById("btn").addEventListener("click", async (e) => {
     e.preventDefault();
-    loader.classList.remove("hide")
-    const transactionID = document.getElementById("journalNumber").value
-    if (transactionID == "") {
-        Swal.fire(
-            '',
-            'Journal Number required!',
-            'error'
-        )
-    } else {
-        const data = {
-            reservationID,
-            name,
-            email,
-            contactNumber,
-            specialRequirement,
-            roomType,
-            checkinDate,
-            checkoutDate,
-            adultNumber,
-            childMinorNumber,
-            childNumber,
-            totalRoomTypes,
-            totalRooms,
-            totalAmount,
-            transactionID
-        }
-        bookNow(data)
-    }
+    sendTransaction()
 })
+
+async function sendXRP(seed, amount, destination) {
+    const client = new xrpl.Client('wss://s.devnet.rippletest.net:51233/', { connectionTimeout: 10000 });
+
+    try {
+        await client.connect();
+        let address = "";
+        let wallet = "";
+        try {
+            wallet = xrpl.Wallet.fromSeed(seed);
+            address = wallet.classicAddress;
+        } catch (err) {
+            console.error("Invalid Seed Value", err);
+            return;
+        }
+        console.log(address)
+        const prepared = await client.autofill({
+            "TransactionType": "Payment",
+            "Account": wallet.address,
+            "Amount": xrpl.xrpToDrops(amount),
+            "Destination": destination
+        });
+        const signed = wallet.sign(prepared);
+        console.log(signed)
+        const tx = await client.submitAndWait(signed.tx_blob).catch((err) => {
+            console.error("Transaction submission error:", err);
+        });
+
+        const transactionID = tx.result.hash
+
+        if (tx.result.meta.TransactionResult === "tesSUCCESS") {
+            const data = {
+                reservationID,
+                name,
+                email,
+                contactNumber,
+                specialRequirement,
+                roomType,
+                checkinDate,
+                checkoutDate,
+                adultNumber,
+                childMinorNumber,
+                childNumber,
+                totalRoomTypes,
+                totalRooms,
+                totalAmount,
+                transactionID
+            }
+            bookNow(data)
+        }
+
+    } catch (error) {
+        console.error(error);
+    } finally {
+        await client.disconnect();
+    }
+}
