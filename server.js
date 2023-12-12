6802d1d18273c0ba7d756657c1411637fece57ee
xeviabcd28 <xeviabcd28@gmail.com>
2023-12-12 22:39:09 +0600

    try

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 const app = require('./app')
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
-dotenv.config({path : './config.env'})
+dotenv.config({ path: './config.env' })
 
 const DB = process.env.DATABASE.replace('PASSWORD',
-process.env.DATABASE_PASSWORD)
+    process.env.DATABASE_PASSWORD)
 
 mongoose.connect(DB).then((con) => {
     console.log('Database Connection Successful')
 }).catch(error => console.log(error));
 
-const port = 4001
+const port = process.env.PORT || 4001;
 app.listen(port, () => {
     console.log(`App running on port ${port}`)
 })
